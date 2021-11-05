28c73c4691c1944c9704093aaa1c40d153d63336
1exyy <alxvagbar@gmail.com>
2021-11-05 13:31:55 +0200

    qwe

--- src/App.js
+++ src/App.js
@@ -1,74 +1,66 @@
 import React, {useState} from 'react';
 import MyButton from "./components/UI/Mybutton/Mybutton";
+import {fromEvent, interval} from 'rxjs';
 import Counter from "./components/counter";
+
 import './App.css';
 
 function App() {
-    let lastClicked = 0;
-
     let isWait = false;
-    let [counterInterval, setIntervalState] = useState('');
-    let [isWork, setIsWork] = useState(false);
+    let [counter, setCounter] = useState('')
     let [timeObject, setTime] = useState({
         seconds: '00',
         minutes: '00',
         hours: '00'
     });
 
-    // Старт / Стоп
-    const counterHandler = () => {
-        if (isWait === false) {
-            if (!isWork) {
-                setIsWork(isWork = true);
-                startCounter()
-            } else {
-                setIsWork(isWork = false);
+    window.onload = () => {
+        const startButton = document.querySelector('#start');
+        const waitButton = document.querySelector('#wait');
+        const resetButton = document.querySelector('#reset');
+
+        fromEvent(startButton, 'click')
+            .subscribe(() => {
+
+                if (isWait) {
+                    counter.isStopped = false;
+                    isWait = false;
+                    return;
+                }
+
+                if (!counter) {
+                    iterator();
+                } else {
+                    setTime(timeObject = {seconds: '00', minutes: '00', hours: '00'});
+                    counter.unsubscribe();
+                    setCounter(counter = false);
+                }
+            });
+
+        fromEvent(waitButton, 'click')
+            .subscribe(() => {
+                if (!isWait) {
+                    counter.isStopped = true;
+                    isWait = true;
+                }
+            });
+
+        fromEvent(resetButton, 'click')
+            .subscribe(() => {
                 setTime(timeObject = {
                     seconds: '00',
                     minutes: '00',
                     hours: '00'
                 });
-                clearInterval(counterInterval);
-            }
-        } else {
-            startCounter();
-        }
-    }
-    //Сброс времени
-    const resetHandler = () => {
-        clearInterval(counterInterval);
-        setIntervalState('')
-        setIsWork(isWork = false);
-        setTime(timeObject = {
-            seconds: '00',
-            minutes: '00',
-            hours: '00'
-        });
-        counterHandler()
+                if (counter.closed) {
+                    counter.closed = true;
+                }
+                iterator()
+            });
     }
 
-    //Отслеживание разницы кликов
-    function checkClickDifference(duration) {
-        let now = new Date().getTime();
-        if (now > (lastClicked + duration)) {
-            lastClicked = now;
-            return true;
-        } else {
-            lastClicked = now;
-            return false;
-        }
-    }
-
-    //Кнопка ожидания
-    const waitHandler = () => {
-        if (!checkClickDifference(300)) {
-            isWait = true;
-            clearInterval(counterInterval);
-        }
-    }
-    //Запуск секундомера
-    const startCounter = () => {
-        setIntervalState(counterInterval = setInterval(() => {
+    function iterator() {
+        setCounter(counter = interval(1000).subscribe((res) => {
             timeObject.seconds++
             if (timeObject.seconds === 60) {
                 timeObject.seconds = 0;
@@ -85,25 +77,25 @@ function App() {
                 minutes: (timeObject.minutes <= 9 && timeObject.minutes > 0) ? '0' + timeObject.minutes : timeObject.minutes,
                 hours: (timeObject.hours <= 9 && timeObject.hours > 0) ? '0' + timeObject.hours : timeObject.hours
             });
-        }, 1000));
+        }))
     }
 
     return (
         <div className="container">
-                <Counter
-                    time={timeObject}
-                />
-                <div className='d-flex-row'>
-                    <MyButton onClick={counterHandler}>
-                        Start / Stop
-                    </MyButton>
-                    <MyButton onClick={waitHandler}>
-                        Wait
-                    </MyButton>
-                    <MyButton onClick={resetHandler}>
-                        Reset
-                    </MyButton>
-                </div>
+            <Counter
+                time={timeObject}
+            />
+            <div className='d-flex-row'>
+                <MyButton id="start">
+                    Start / Stop
+                </MyButton>
+                <MyButton id="wait">
+                    Wait
+                </MyButton>
+                <MyButton id="reset">
+                    Reset
+                </MyButton>
+            </div>
         </div>
     );
 }
